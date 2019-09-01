152bbc5813cd0e86d30d26e9c54b686f71ee30d0
George Moura <gwmoura@gmail.com>
2019-09-01 10:06:19 -0300

    setup inicial

---
+++ migrate.js
@@ -0,0 +1,12 @@
+var path = require('path');
+
+const config = {
+    migrationsDir: path.resolve(process.cwd(), 'mysql-migrations'),
+    adapter: 'mysql',
+    host: process.env.DB_HOST,
+    db: process.env.DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+}
+require('sql-migrations').run(config);
