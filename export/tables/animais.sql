1402c1924a16eac919633dc3bdd35ad6f71ae9e6
George Moura <gwmoura@gmail.com>
2020-03-07 18:06:17 -0300

    adicionando data de nascimento na tabela de animais

--- export/tables/animais.sql
+++ export/tables/animais.sql
@@ -3,5 +3,6 @@ CREATE TABLE `animais` (
   `nome` varchar(200) NOT NULL,
   `data_criacao` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `idade` int(11) DEFAULT NULL,
+  `data_nascimento` date DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
